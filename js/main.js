60c327b78d4d962a95ea6ffa7f6dde964694b7e4
Dario Chic <98721450+Dario-Chic@users.noreply.github.com>
2022-02-05 17:48:48 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,217 +1,161 @@
-let form = document.getElementById("form");
-let inputsForm = form.getElementsByTagName("input");
-let error = document.getElementById("error");
+var form = document.getElementById("form");
+var inputsForm = form.getElementsByTagName("input");
+var error = document.getElementById("error");
 
-let userFirtsName = document.getElementById("firtsName");
-let userLastName = document.getElementById("lastName");
-let userAge = document.getElementById("age");
-let userCountry = document.getElementById("country");
-let userAmount = 0;
-
+var userFirtsName = document.getElementById("firtsName");
+var userLastName = document.getElementById("lastName");
+var userAge = document.getElementById("age");
+var userCountry = document.getElementById("country");
 const addNewUser = document.getElementById("add-button");
 const delNewUser = document.getElementById("del-button");
+var userAmount = 0;
 
-let rowContainer = document.getElementById("table-body");
-let rowElements = rowContainer.getElementsByTagName("tr");
+addNewUser.addEventListener("click", validation);
+delNewUser.addEventListener("click", delUser);
+document.addEventListener("keydown", function enter(key) {
+  for (inputs in inputsForm) {
+    if (key.keyCode == 13 && document.activeElement == inputsForm[inputs]) {
+      validation();
+    }
+  }
+});
 
-let btns = document.getElementsByClassName("fa-user-times");
-let btnEdit = document.getElementsByClassName("fa-edit");
+var rowContainer = document.getElementById("table-body");
+var rowElements = rowContainer.getElementsByTagName("tr");
+var padre;
 
-var onlyOne = 0;
+var btns = document.getElementsByClassName("fa-user-times");
 
 function validation() {
-	// !--------------------------------
-	function check() {
-		if (userFirtsName.value == "" || userLastName.value == "" || userAge.value == "" || userCountry.value == "") {
-			notification();
-		} else {
-			error.style.display = "none";
-			addUser();
-		}
-	}
-	function notification() {
-		let acc = 0;
+  error.innerHTML = "";
+  activation();
 
-		let notifications = ["<li>Please, write a Name</li>", "<li>Please, write a Last Name</li>", "<li>Please, write an Age</li>", "<li>Please, write a Country</li>"];
+  function activation() {
+    if (
+      userFirtsName.value == "" ||
+      userLastName.value == "" ||
+      userAge.value == "" ||
+      userCountry.value == ""
+    ) {
+      notification();
+    } else {
+      error.style.display = "none";
+      addUser();
+    }
+  }
+  function notification() {
+    if (userFirtsName.value == "" || userFirtsName.value == null) {
+      error.style.display = "block";
+      error.innerHTML += "<li>Please, write a name.</li>";
+    }
 
-		for (element in userData) {
-			if (userData[element] == "") {
-				error.innerHTML += notifications[element];
-				acc++;
-			} else if (acc == 4) {
-				console.log("add");
-			}
-			error.style.display = "block";
-		}
-		// }
-	}
-	//
-	let userData = [userFirtsName.value, userLastName.value, userAge.value, userCountry.value];
-	error.innerHTML = "";
-	check();
-	// !-------------------------------
+    if (userLastName.value == "" || userLastName.value == null) {
+      error.style.display = "block";
+      error.innerHTML += "<li>Please, write a Last Name</li>";
+    }
+
+    if (userAge.value == "" || userAge.value == null) {
+      error.style.display = "block";
+      error.innerHTML += "<li>Please, write an age</li>";
+    }
+
+    if (userCountry.value == "" || userCountry.value == null) {
+      error.style.display = "block";
+      error.innerHTML += "<li>Please, write a country</li>";
+    }
+  }
 }
 
 function addUser() {
-	// !-------------------------------
-	userAmount++;
+  var userData = [];
+  // * At firts time we obtain every inputs value
+  for (value in inputsForm) {
+    userData.push(inputsForm[value].value);
+  }
 
-	let userData = [userFirtsName.value, userLastName.value, userAge.value, userCountry.value];
+  // ! There is a bug that makes 3 elements more instead only 4, i never knew what happened, so i fixed it with this code, it works good now
+  while (userData.length > 4) {
+    userData.pop();
+  }
 
-	let classes = ["userName", "userLastName", "userAge", "userCountry"];
+  // ** At this point i created a necessary 'tr' to add users data as 'td' after
+  var newRow = document.createElement("tr");
+  userAmount = rowElements.length;
+  newRow.id = "user" + userAmount; //* it works to assign an ordered list via numbers id
+  rowContainer.appendChild(newRow);
 
-	let newRow = document.createElement("tr");
-	newRow.id = "user" + userAmount;
-	rowContainer.appendChild(newRow);
+  // ** this section works adding 'tr' elements with users data into 'td' created before
+  for (data in userData) {
+    var dataSelect = document.createTextNode(
+      userData[data].charAt(0).toUpperCase() +
+        userData[data].slice(1).toLowerCase()
+    );
 
-	for (data in userData) {
-		let dataSelect = document.createTextNode(userData[data].charAt(0).toUpperCase() + userData[data].slice(1).toLowerCase());
+    var rowData = document.createElement("td");
+    rowData.appendChild(dataSelect);
 
-		let rowData = document.createElement("td");
-		rowData.appendChild(dataSelect);
+    var rowDataContainer = document.getElementById(newRow.id);
+    rowDataContainer.appendChild(rowData);
 
-		let rowDataContainer = document.getElementById(newRow.id);
-		rowDataContainer.appendChild(rowData);
+    // ** this section works adding classname and id into 'tr'
+    if (userData[data] == userFirtsName.value) {
+      rowData.className = "userName";
+      rowData.id = "userName" + userAmount;
+      //
+    } else if (userData[data] == userLastName.value) {
+      rowData.className = "userLastName";
+      rowData.id = "userLastName" + userAmount;
+      btnID = rowData.id;
+      //
+    } else if (userData[data] == userAge.value) {
+      rowData.className = "userAge";
+      rowData.id = "userAge" + userAmount;
+      //
+    } else if (userData[data] == userCountry.value) {
+      rowData.className = "userCountry";
+      rowData.id = "userCountry" + userAmount;
+    }
+  }
 
-		rowData.className = classes[data];
-		rowData.id = classes[data] + userAmount;
-		addEditBtn(rowData.id);
-	}
-	addDelBtn();
-	userFirtsName.value = "";
-	userLastName.value = "";
-	userAge.value = "";
-	userCountry.value = "";
-	// !-------------------------------
+  addDelBtn();
+  // ** Reset all items to initial settings
+  userFirtsName.value = "";
+  userLastName.value = "";
+  userAge.value = "";
+  userCountry.value = "";
+  userData = [];
 }
 
 function delUser() {
-	// !-------------------------------
-	var userSelect = userAmount - 1;
-
-	rowContainer.removeChild(rowElements[userSelect]);
-	toFixOrder();
-	// !-------------------------------
+  userAmount = rowElements.length;
+  var userSelect = userAmount - 1;
+  rowContainer.removeChild(rowElements[userSelect]);
+  toFixOrder();
 }
 
 function addDelBtn() {
-	// !-------------------------------
-	let btn = document.createElement("i");
-	btn.className = `fas fa-user-times`;
-	let btnContainer = document.getElementById("userName" + userAmount);
-	btnContainer.appendChild(btn);
-
-	btn.addEventListener("click", function () {
-		let father = this.parentNode;
-		let grandfather = father.parentNode;
-		let grandgrandfather = grandfather.parentNode;
-		grandgrandfather.removeChild(grandfather);
-		toFixOrder();
-	});
-	// !-------------------------------
+  var btn = document.createElement("i");
+  btn.className = `fas fa-user-times`;
+  var btnContainer = document.getElementById("userName" + userAmount);
+  btnContainer.appendChild(btn);
+  btn.addEventListener("click", delBtn);
 }
 
-function addEditBtn(id) {
-	// !-------------------------------
-	let btn = document.createElement("i");
-	btn.className = `far fa-edit`;
-	let btnContainer = document.getElementById(id);
-	btnContainer.appendChild(btn);
-
-	btn.addEventListener("click", openEdit);
+function delBtn() {
+  var father = this.parentNode;
+  var grandfather = father.parentNode;
+  var grandgrandfather = grandfather.parentNode;
+  grandgrandfather.removeChild(grandfather);
+  toFixOrder();
 }
 
-function openEdit() {
-	if (onlyOne == 0) {
-		onlyOne = 1;
-		let container = this.parentNode;
-		let containerID = container.id;
-		let valueBackup = container.innerHTML;
-
-		let backupReady = valueBackup.replace(`<i class="far fa-edit"></i><i class="fas fa-user-times"></i><i class="far fa-edit"></i>`, "");
-		container.innerHTML = null;
-
-		let newDiv = document.createElement("div");
-		newDiv.className = `edit`;
-		newDiv.id = `edit`;
-		newDiv.innerHTML = `<input type="text" class="text-edit" id="data-edit" placeholder="Edita...">
-             <button class="inputs-name__add-button" id="accept-edit-button">Accept</button>
-               <button class="inputs-name__del-button" id="cancel-edit-button">Cancel</button>`;
-
-		container.appendChild(newDiv);
-
-		let edit = document.getElementById(`edit`);
-		edit.style.display = "grid";
-
-		let accept = document.getElementById("accept-edit-button");
-		let newData = document.getElementById("data-edit");
-
-		function acceptEdit() {
-			if (newData.value == "" || newData.value == null) {
-				cancelEdit();
-			} else {
-				let add = newData.value.charAt(0).toUpperCase() + newData.value.slice(1).toLowerCase();
-				container.innerHTML = `${add}`;
-				addDelBtn(containerID);
-				addEditBtn(containerID);
-				edit.style.display = "none";
-				onlyOne = 0;
-			}
-		}
-
-		function cancelEdit() {
-			container.innerHTML = backupReady;
-			addDelBtn(containerID);
-			addEditBtn(containerID);
-			edit.style.display = "none";
-			onlyOne = 0;
-		}
-
-		accept.addEventListener("click", acceptEdit);
-
-		document.addEventListener("keydown", function (key) {
-			if (key.keyCode == 13 && document.activeElement == newData) acceptEdit();
-		});
-
-		let cancel = document.getElementById("cancel-edit-button");
-
-		cancel.addEventListener("click", cancelEdit);
-	}
-}
-
+var user;
 function toFixOrder() {
-	// !-------------------------------
-	for (tr in rowElements) {
-		rowElements[tr].id = "user" + ++tr;
-	}
-	userAmount--;
-	toFixId();
-	// !-------------------------------
+  for (tr in rowElements) {
+    rowElements[tr].id = "user" + tr;
+  }
+  userAmount = rowElements.length;
 }
-
-function toFixId() {
-	let classes = ["userName", "userLastName", "userAge", "userCountry"];
-
-	for (tr in rowElements) {
-		let padreActual = document.getElementById(rowElements[tr].id);
-		let actual = padreActual.getElementsByTagName("td");
-		console.log(actual[2]);
-		let number = parseInt(tr) + 1;
-
-		for (td in actual) {
-			actual[td].id = classes[td] + number;
-		}
-	}
-}
-
-addNewUser.addEventListener("click", validation);
-
-delNewUser.addEventListener("click", delUser);
-
-document.addEventListener("keydown", function (key) {
-	for (inputs in inputsForm) {
-		if (key.keyCode == 13 && document.activeElement == inputsForm[inputs]) {
-			validation();
-		}
-	}
-});
+//         var user[td].id = 'user' + tr;
+//     }
+// }
